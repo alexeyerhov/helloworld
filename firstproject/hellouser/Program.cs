bcbae18fca716af7db99db6070e6571ba175378c
Alexey <>
2023-09-24 16:21:33 +0300

    added gitignore

---
+++ firstproject/hellouser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hellouser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName);
+        }
+    }
+}
