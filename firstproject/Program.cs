981cd09aca2b95b027f3d3960ed1b746dcba4f7f
Alexey <>
2023-09-24 15:54:58 +0300

    Test

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -6,7 +6,7 @@ namespace firstproject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("2 Hello World!");
+            Console.WriteLine("3 Hello World!");
         }
     }
 }
